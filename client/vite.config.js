d960336d4eb4f8934e478a28b176826c3424d349
Godson Ajodo <godsonajodo@Godsons-MacBook-Pro.local>
2024-10-11 21:06:50 -0700

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+//import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   base: '/NASASpaceAppsChallenge2024/',
-});
+  server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
+  },
+  preview: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
+  },
+})
